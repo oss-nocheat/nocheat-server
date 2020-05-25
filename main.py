be020a72d45b91ff8c4cd4d4050eadba4a4bf35d
k <dntjr8989@naver.com>
2020-05-26 04:54:33 +0900

    5/25~5/26 soundcheck 및 소리 테스트

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
-from StudentClient import StudentClient
+import numpy as np
+import pylab
+from scipy.io.wavfile import write
+from scipy.io import wavfile
+import scipy.io.wavfile
+import os
+from soundcheck import Soundcheck
+import wave
+import matplotlib.pyplot as plt
+f = Soundcheck()
 
-# initialize flask app
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-# initialize socket io
-socketio = SocketIO(app, cors_allowed_origins="*")
-# connected student object list
-student_clients = list()
+# sampling rate
+Fs = 44100.0  # Hz
+
+# play length
+tlen = 10  # s
+Ts = 1 / Fs  # sampling interval
+t = np.arange(0, tlen, Ts)  # time array
+
+sin_freq = 22000# Hz
+signal = np.sin(2 * np.pi * sin_freq * t)
+
+sin_freq3 = 1400 #Hz
+signal3 = np.sin(2*np.pi*sin_freq3*t)
+
+sin_freq2 = 22000 # to make Hz people cannot hear
+signal2 = np.sin(2*np.pi*sin_freq2*t)
+# generate noise
+signal_n = signal + 10000000*signal2 + 100*signal3
+
+signal_f = np.fft.fft(signal_n)
+freq = np.fft.fftfreq(len(t), Ts)
+
+pylab.plot(freq, 20 * np.log10(np.abs(signal_f)))
+pylab.xlim(0, Fs / 2)
+pylab.show()
+
+#makesignal_f = np.fft.fft(signal_n)
+makefreq = [440, 880, 22000]
+
+fs, data = scipy.io.wavfile.read('test10.wav')
+
+signal_f = np.fft.fft(data)
+
+freq = np.linspace(0, fs, len(20*np.log10(np.abs(signal_f))))
+
+pylab.plot(freq, 20 * np.log10(np.abs(signal_f)))
+pylab.xlim(0, Fs / 2)
+pylab.show()
+
+print(freq)
+print(signal_f)
+
+f.soundanalysis(freq, signal_f, makefreq)
+
+print(f.cntvalue())
+print(f.cheatcntvalue())
 
 
-# when client connected
-@socketio.on('message')
-def handle_client_connect(data):
-    print(f'client {data} connected')
-    student_clients.append(StudentClient(data))
-
-
-@app.route('/')
-def index():
-    return '''
-    hello world!
-    '''
-
-@app.route('/play_sample')
-def play_sample():
-    socketio.emit('play')
-
-if __name__ == '__main__':
-    socketio.run(app)
 
---
+++ soundcheck.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pylab
+from scipy.io.wavfile import write
+import os
+
+class Soundcheck:
+    cnt = 0
+    cheatcnt = 0
+    def __init__(self):
+        self.cnt = 0
+        self.cheatcnt = 0
+
+    def soundanalysis(self, freq, signal_f, makefreq):
+        for i in range(len(freq)) :
+            if (20*np.log10(np.abs(signal_f[i]))) >= 100 :
+                for j in range(len(makefreq)) : 
+                    if freq[i]  <= makefreq[j] * 1.0005 and freq[i] >= makefreq[j] * 0.0095:
+                        self.cnt+=1
+                        print('<')
+                        print(freq[i])
+                        break
+        if len(makefreq) == self.cnt :
+            self.cheatcnt+=1 
+    
+    def cntzero(self):
+        self.cnt = 0
+    def cntvalue(self):
+        return self.cnt
+    def cheatcntvalue(self):
+        return self.cheatcnt
+    
+
