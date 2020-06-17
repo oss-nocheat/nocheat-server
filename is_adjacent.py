cedf1218e5b6e319f0ca1279483de5b8526cd98f
k <dntjr8989@naver.com>
2020-06-18 03:13:42 +0900

    final promotion  sound part

--- is_adjacent.py
+++ is_adjacent.py
@@ -8,20 +8,28 @@ from soundcheck import Soundcheck
 import wave
 import matplotlib.pyplot as plt
 
-def is_adjacent(sound, makefreq, f, makedb) :
-    
+
+def is_adjacent(sound, makefreq, f, makedb):
     fs, data = scipy.io.wavfile.read(sound)
 
-    print(fs, data)
+    # print(fs, data)
     signal_f = np.fft.fft(data)
-    print(signal_f)
+    # print(signal_f)
 
-    freq = np.linspace(0, fs, len(20*np.log10(np.abs(signal_f))))
+    freq = np.linspace(0, fs, len(20 * np.log10(np.abs(signal_f))))
 
     Fs = 44100.0
     pylab.plot(freq, 20 * np.log10(np.abs(signal_f)))
-    pylab.xlim(0, Fs/2)
+    pylab.xlim(0, Fs / 2)
     pylab.show()
 
     f.soundanalysis(freq, signal_f, makefreq, makedb)
 
+    print(sound, end=' : ')
+    if f.cheatcntvalue() == 1:
+        print("Detect cheating")
+    else:
+        if f.cntvalue() == 4:
+            print("Doubt cheating")
+        else:
+            print("Innocence")
--- soundcheck.py
+++ soundcheck.py
@@ -3,9 +3,11 @@ import pylab
 from scipy.io.wavfile import write
 import os
 
+
 class Soundcheck:
     cnt = 0
     cheatcnt = 0
+
     def __init__(self):
         self.cnt = 0
         self.cheatcnt = 0
@@ -15,49 +17,50 @@ class Soundcheck:
         dbsum = 0
         datacnt = 0
         dbmeanarray = []
-        for i in range(len(freq)) :
-            if (20*np.log10(np.abs(signal_f[i]))) >= 70 :
-                for j in range(self.cnt, len(makefreq)) : 
+        for i in range(len(freq)):
+            if (20 * np.log10(np.abs(signal_f[i]))) >= 90:
+                for j in range(self.cnt, len(makefreq)):
 
-                    print(freq[i], makefreq[j], flag, self.cnt)
-                    if freq[i]  <= makefreq[j] * 1.00005 and freq[i] >= makefreq[j] * 0.99995  :
+                    # print(freq[i], makefreq[j], flag, self.cnt, (20*np.log10(np.abs(signal_f[i]))))
+                    if freq[i] <= makefreq[j] * 1.0005 and freq[i] >= makefreq[j] * 0.9995:
                         flag = j
-                        dbsum = dbsum + 20*np.log10(np.abs(signal_f[i]))
+                        dbsum = dbsum + 20 * np.log10(np.abs(signal_f[i]))
                         datacnt = datacnt + 1
-                        print(freq[i], makefreq[j])
-                        os.system("Pause")
-                    else :
-                        if flag != -1 :
-                            self.cnt+=1   
-                            if datacnt != 0 :
-                                dbmeanarray.append(dbsum/datacnt)
+                        # print(freq[i], makefreq[j])
+                        # os.system("Pause")
+                    else:
+                        if flag != -1:
+                            self.cnt += 1
+                            if datacnt != 0:
+                                dbmeanarray.append(dbsum / datacnt)
                             flag = -1
                             datacnt = 0
                             dbsum = 0
-                            os.system("Pause")
-                            
+                            # os.system("Pause")
 
-        if len(makefreq) == self.cnt :
+        if len(makefreq) == self.cnt:
             flagt = 0
-            for i in range(len(makefreq)) :
-                for j in range(i+1 ,len(makefreq)) :
-                    if makefreq[i] <= makefreq[j] and dbmeanarray[i] <= dbmeanarray[j] :
+            for i in range(len(makefreq)):
+                for j in range(i + 1, len(makefreq)):
+                    # print(i, makedb[i], dbmeanarray[i], j, makedb[j], dbmeanarray[j])
+                    if makedb[i] <= makedb[j] and dbmeanarray[i] <= dbmeanarray[j]:
                         continue
-                    elif makefreq[i] >= makefreq[j] and dbmeanarray[i] >=dbmeanarray[j] :
+                    elif makedb[i] >= makedb[j] and dbmeanarray[i] >= dbmeanarray[j]:
                         continue
-                    else :
+                    else:
                         flagt = 1
                         break
-                if flagt == 1 :
+                if flagt == 1:
                     break
-            if flagt == 0 :
-                self.cheatcnt+=1 
-    
+            if flagt == 0:
+                self.cheatcnt += 1
+
     def cntzero(self):
         self.cnt = 0
+
     def cntvalue(self):
         return self.cnt
+
     def cheatcntvalue(self):
         return self.cheatcnt
-    
 
