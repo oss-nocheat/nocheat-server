201aa51baac2a8f3b0b241d75277d851417ad8df
gkrry2723 <51503799+gkrry2723@users.noreply.github.com>
2020-06-26 23:57:24 +0900

    coding convention rule에 맞게 수정!

--- Exam.py
+++ Exam.py
@@ -1,8 +1,11 @@
 class Exam:
     def __init__(self, instructor, name):
+        
         self.name = name
         self.instructor = instructor
         self.examinee = []
 
+        
     def add_examinee(self, examinee):
+        
         self.examinee.append(examinee)
