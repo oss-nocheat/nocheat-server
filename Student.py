67f4af9eb2c579992c30fb80a1d6b5025695af9e
gkrry2723 <51503799+gkrry2723@users.noreply.github.com>
2020-06-26 23:58:00 +0900

    coding convention rule 적용

--- Student.py
+++ Student.py
@@ -1,5 +1,7 @@
 class Student:
+    
     def __init__(self, session_id, student_id, name):
+        
         self.session_id = session_id
         self.student_id = student_id
         self.name = name
